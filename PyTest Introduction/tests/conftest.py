4845433f209dc7ccef85381ed78dbc22034af731
Olha Karpenko <olhalilu@gmail.com>
2026-03-16 18:55:27 +0200

    Remove __pycache__ and .pyc files from repository

--- PyTest Introduction/tests/conftest.py
+++ PyTest Introduction/tests/conftest.py
@@ -3,16 +3,22 @@ import pandas as pd
 import os
 
 # Fixture to read the CSV file
-@pytest.fixture()
+@pytest.fixture(scope='session')
 def read_file():
-    test_dir = os.path.dirname(__file__)
-    project_root = os.path.abspath(os.path.join(test_dir, '..'))
+    test_dir = os.path.dirname(__file__)  # tests/
+    project_root = os.path.abspath(os.path.join(test_dir, '..'))  # PyTest Introduction/
     file_path = os.path.join(project_root, 'src', 'data', 'data.csv')
-    with open(file_path, 'r') as f:
-        first_line = f.readline()
-    return first_line
+    df = pd.read_csv(file_path)
+    return df
 
 # Fixture to validate the schema of the file
+@pytest.fixture(scope='session',params = ['actual_schema','expected_schema'])
+def validate_schema(read_file):
+    actual_schema = list(read_file.columns)
+    expected_schema = ['id', 'name', 'age', 'email', 'is_active']
+    return actual_schema, expected_schema
+
+
 
 
 # Pytest hook to mark unmarked tests with a custom mark
--- PyTest Introduction/tests/test_csv/test_csv_file.py
+++ PyTest Introduction/tests/test_csv/test_csv_file.py
@@ -2,20 +2,25 @@ import pytest
 import re
 
 
-def test_file_not_empty():
-    assert 1 + 1 == 2
+import os
+
+def test_file_not_empty(read_file):
+    assert not read_file.empty
 
 
 def test_duplicates():
     assert 1 + 1 == 2
 
+@pytest.mark.validate_csv
+def test_validate_schema(validate_schema):
+    actual_schema,expected_schema = validate_schema
+    assert actual_schema==expected_schema
 
-def test_validate_schema():
-    assert 1 + 1 == 2
-
-
-def test_age_column_valid():
-    assert 1 + 1 == 2
+@pytest.mark.validate_csv
+@pytest.mark.skip(reason="Not implemented yet")
+def test_age_column_valid(read_file):
+    condition = read_file.age.between(0, 100)
+    assert condition.all(),'invalid age'
 
 
 def test_email_column_valid():
