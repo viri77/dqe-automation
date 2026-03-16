87d525a3ecf1e8dd9cc209114bf28321e165738a
Olha Karpenko <olhalilu@gmail.com>
2026-03-16 18:51:36 +0200

    added 3 tests

--- PyTest Introduction/tests/conftest.py
+++ PyTest Introduction/tests/conftest.py
@@ -1,8 +1,16 @@
 import pytest
 import pandas as pd
+import os
 
 # Fixture to read the CSV file
-
+@pytest.fixture()
+def read_file():
+    test_dir = os.path.dirname(__file__)
+    project_root = os.path.abspath(os.path.join(test_dir, '..'))
+    file_path = os.path.join(project_root, 'src', 'data', 'data.csv')
+    with open(file_path, 'r') as f:
+        first_line = f.readline()
+    return first_line
 
 # Fixture to validate the schema of the file
 
