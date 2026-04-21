a7cdb0564294c2688b2a4d12872b1c237891817c
Olha Karpenko <viri77557@gmail.com>
2026-04-21 20:19:22 +0300

    selenium

---
+++ Selenium Introduction/README.md
@@ -0,0 +1,23 @@
+# Clone the repository (if needed)
+git clone https://github.com/viri77/dqe-automation.git
+# Change to your project directory
+cd "Selenium Introduction" 
+
+## Setup
+
+# Create virtual environment
+Mac
+python3 -m venv .venv
+
+windows
+python -m venv venv
+
+# Activate virtual environment
+Mac
+source .venv/bin/activate
+
+Windows
+venv\Scripts\activate
+
+# Install requirements
+pip install -r requirements.txt
--- Selenium Introduction/main.py
+++ Selenium Introduction/main.py
@@ -1,6 +1,8 @@
+import selenium
 import time
 import os
-import pandas as pd
+import csv
+
 
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
@@ -8,18 +10,128 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
-
+from selenium.common.exceptions import WebDriverException
 
 class SeleniumWebDriverContextManager:
     def __init__(self):
+        self.driver = None
+
 
     def __enter__(self):
+        try:
+            self.driver = webdriver.Chrome()
+            self.driver.maximize_window()
+            return self.driver
+        except  WebDriverException as e:
+                print(f"launch webdriver exception: {e}")
+        except  TimeoutException as e:
+                print(f"timeoute exception: {e}")
+                raise
 
     def __exit__(self, exc_type, exc_value, traceback):
+            if self.driver:
+                self.driver.quit()
 
 
 if __name__ == "__main__":
     with SeleniumWebDriverContextManager() as driver:
-        # file_path = ...
-        # drivet.get(...)
-        # ...
+        file_path = "report.html"
+        abs_path = os.path.abspath(file_path)
+        url = f"file:///{abs_path.replace(os.sep, '/')}"
+        driver.get(url)
+        time.sleep(5)
+        try:
+            table_element = driver.find_element(By.CLASS_NAME, "table")
+            columns = table_element.find_elements(By.CLASS_NAME, "y-column")
+        except NoSuchElementException as e:
+            print(f"No SuchElement : {e}")
+            raise
+
+        headers = []
+        values = []
+
+        for col in columns:
+            header_blocks = col.find_elements(By.ID, 'header')
+            for block in header_blocks:
+                headers.append(block.text)
+            if col.get_attribute("id") != "header":
+                values.append(col.text)
+
+        data_columns = [col.split('\n') for col in values]
+
+        num_rows = min((len(col)-1) for col in data_columns) if data_columns else 0
+
+#writing table to CSV
+
+        csv_file = "table.csv"
+        with open(csv_file, mode="w", newline='', encoding="utf-8") as f:
+            writer = csv.writer(f)
+            writer.writerow(headers)
+            for i in range(num_rows):
+                row = [data_columns[j][i] for j in range(len(data_columns))]
+                writer.writerow(row)
+
+        print(f"Saved to: {csv_file}")
+
+        #chart
+
+        chart = driver.find_element(By.CLASS_NAME, "pielayer")
+        chart.screenshot("screenshot0.png")
+
+        #pychart screenshot initial state
+
+        texts = driver.find_elements(By.CSS_SELECTOR, "text.slicetext")
+        if not texts:
+            raise NoSuchElementException("No slicetext elements found")
+        data = []
+        for t in texts:
+            raw = t.get_attribute("data-unformatted")
+            if raw:
+                parts = raw.replace('&lt;br&gt;', '\n').replace('<br>', '\n').split('\n')
+                if len(parts) >= 2:
+                    label = parts[0].strip()
+                    value = parts[1].strip()
+                    data.append([label, value])
+
+        with open("doughnut0.csv", mode="w", newline='', encoding="utf-8") as f:
+            writer = csv.writer(f)
+            writer.writerow(["Facility Type", "Min average time spent"])
+            writer.writerows(data)
+        try:
+            donut = driver.find_elements(By.CLASS_NAME, "legendtoggle")
+            labels = driver.find_elements(By.CLASS_NAME, "legendtext")
+        except NoSuchElementException as e :
+            print("no donut found" ,{e})
+            raise
+        for i in range(len(labels)):
+            try:
+                donut[i].click()
+                time.sleep(2)
+                chart = driver.find_element(By.CLASS_NAME, "pielayer")
+                chart.screenshot(f"screenshot{i + 1}.png")
+            except WebDriverException as e:
+                driver.save_screenshot(f"screenshot{i + 1}.png")
+
+            time.sleep(1)
+            try:
+                texts = driver.find_elements(By.CSS_SELECTOR, "text.slicetext")
+                if not texts:
+                    raise NoSuchElementException("No slicetext elements found")
+                data = []
+                for t in texts:
+                    raw = t.get_attribute("data-unformatted")
+                    if not raw:
+                        raise NoSuchElementException("No data found")
+                    elif raw:
+                        parts = raw.replace('&lt;br&gt;', '\n').replace('<br>', '\n').split('\n')
+                        label = parts[0].strip()
+                        value = parts[1].strip()
+                        data.append([label, value])
+            except Exception as e:
+                pass
+            with open(f"doughnut{i + 1}.csv", mode="w", newline='', encoding="utf-8") as f:
+                writer = csv.writer(f)
+                writer.writerow(["Facility Type", "Min average time spent"])
+                writer.writerows(data)
+
+            time.sleep(5)
